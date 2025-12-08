ecbe76a573247b4a7ff6bb09f16055270d902696
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2025-12-08 16:14:46 +0700

    cros

--- lms-backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ lms-backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://elearninggg-ij7xicdha-tuancapoos-projects.vercel.app","http://localhost:4174","http://localhost:4173","http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://elearning-khaki.vercel.app","https://elearninggg-ij7xicdha-tuancapoos-projects.vercel.app","http://localhost:4174","http://localhost:4173","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","Accept","x-no-retry"));
         configuration.setAllowCredentials(true);
